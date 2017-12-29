ba240b9d8286aba14f30a40a7c6b75ff5cf06f99
eugeneloza <zhloza@gmail.com>
2017-12-29 07:45:15 +0200

    Add comments and GPL statement add WorldUnit

--- duungeeon.pas
+++ duungeeon.pas
@@ -1,10 +1,29 @@
-program duungeeon;
+{Copyright (C) 2012-2017 Yevhen Loza
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.}
+
+{---------------------------------------------------------------------------}
+
+{ main file }
+
+program Duungeeon;
 
 uses
   SysUtils,
   CastleWindow, CastleSceneCore, CastleScene, CastleLog, CastleFilesUtils,
   X3DLoad, X3DNodes, CastleRandom, CastleVectors, CastleCameras,
-  CastleKeysMouse, MyLoad3D;
+  CastleKeysMouse, MyLoad3D, worldunit;
 
 const
   MapSizeX = 30;
