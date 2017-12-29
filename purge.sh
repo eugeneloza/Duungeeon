66a00b850a5cc454d6f961d025dd1484474ef3d0
eugeneloza <zhloza@gmail.com>
2017-12-29 07:45:20 +0200

    add purge.sh

---
+++ purge.sh
@@ -0,0 +1,11 @@
+# Remove compiler garbage, backups and etc.
+
+rm *.dbg
+find -type d -name 'backup' -prune -exec rm -rf {} \;
+find -type d -name 'lib' -prune -exec rm -rf {} \;
+
+# Remove ugly Linux-related bug with NTFS filesystem
+
+find . -name '.fuse_hidden*' -delete
+
+rm Duungeeon
